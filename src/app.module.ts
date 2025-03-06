c5f1eef852473f1a47381c1605dd51d649371d6f
kavalverde <79605050+kavalverde@users.noreply.github.com>
2025-03-06 17:16:12 -0500

    server

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,20 @@ import { AriModule } from './ari/ari.module';
 import { OpenaiModule } from './openai/openai.module';
 import { CallsModule } from './calls/calls.module';
 import configuration from './config/configuration';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), 'temp') || '',
+      serveRoot: '/audio',
+      // Opcional: configuraciones adicionales
+      serveStaticOptions: {
+        index: false, // No mostrar listado de archivos
+        maxAge: '1d' // Cache de 1 día
+      }
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration],
